dc2b09333a3bbc30377aa17de01c4132690fefce
Bhaga55 <136149268+Bhaga55@users.noreply.github.com>
2023-09-18 11:28:47 +0530

    Add files via upload
    UI Project

---
+++ UI PRoject.js
@@ -0,0 +1,141 @@
+
+import './App.css';
+ 
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+
+
+function App(){
+
+  
+   return(
+
+    <div >
+      <p classname="aa">Medico Sales</p>
+      <div className="container">
+    <input classname="aaa" type="text" placeholder="search"/>
+    <Button className="bbb" variant="secondary" size="lg" disabled>Lap Test</Button>
+    <Button className="ccc" variant="secondary" size="lg" disabled>
+       Dashboard
+      </Button>
+      <Button className="ZZZ" variant="secondary" size="lg" disabled>
+        Appointment
+      </Button>
+      <Button classname="bb" variant="secondary" size="lg" disabled>
+        Medical Order 
+      </Button>
+      <Button className="cc" variant="secondary" size="lg" disabled>
+        Message
+      </Button>
+      <Button  className="dd" variant="secondary" size="lg" disabled>
+        Payment  
+      </Button>
+      <Button  className="ee" variant="secondary" size="lg" disabled>
+        Settings
+      </Button>
+      </div>
+
+      <div className="container2">
+
+
+      <div classname="XXX">Sales Information</div>
+
+      <span className="ff"> Customer</span>
+      <input classname="AAA" type="text" placeholder="Enter Customer Name"/>
+         <span className="gg"> Invoice Id </span>
+         <input classname="BBB" type="text" placeholder="Enter Invoice ID"/>
+
+             <span className="hh">   Start Date </span> 
+             <input classname="CCC" type="text" placeholder="Start Date"/>
+         <span className="ii">     End Date </span>
+         <input classname="DDD" type="text" placeholder="End Date"/>
+         </div>
+      <div className="container1">
+      <div className="aaaaaa">Invoice Id</div>
+      <div className="bbbbbb">Date</div>
+      <div className="cccccc">Customer</div>
+      <div className="dddddd">Payable Amount</div>
+      <div className="eeeeee">Paid Amount</div>
+      <div className="ffffff">Due</div>
+        <div className="jj">#AHGA68</div>
+        <div className="kk">#AHGA68</div>
+        <div className="ll">#AHGA68</div>
+        <div className="mm">#AHGA68</div>
+        <div className="nn">#AHGA68</div>
+        <div className="oo">#AHGA68</div>
+        <div className="pp">#AHGA68</div>
+        <div className="qq">#AHGA68</div>
+        <div className="rr">#AHGA68</div>
+        <div className="ss">23/09/2023</div>
+        <div className="tt">23/09/2023</div>
+        <div className="uu">23/09/2023</div>
+        <div className="vv">23/09/2023</div>
+        <div className="ww">23/09/2023</div>
+        <div className="xx">23/09/2023</div>
+        <div className="yy">23/09/2023</div>
+        <div className="zz">23/09/2023</div>
+        <div className="cccc">23/09/2023</div>
+        <div className="dddd">Jacob Marcus</div>
+        <div className="eeee">Jacob Marcus</div>
+        <div className="ffff">Jacob Marcus</div>
+        <div className="gggg">Jacob Marcus</div>
+        <div className="hhhh">Jacob Marcus</div>
+        <div className="iiii">Jacob Marcus</div>
+        <div className="jjjj">Jacob Marcus</div>
+        <div className="kkkk">Jacob Marcus</div>
+        <div className="llll">Jacob Marcus</div>
+        <div className="mmmm">$100</div>
+        <div className="nnnn">$100</div>
+        <div className="oooo">$100</div>
+        <div className="pppp">$100</div>
+        <div className="qqqq">$100</div>
+        <div className="rrrr">$100</div>
+        <div className="ssss">$100</div>
+        <div className="tttt">$100</div>
+        <div className="uuuu">$100</div>
+        <div className="vvvv">$000</div>
+        <div className="wwww">$000</div>
+        <div className="xxxx">$000</div>
+        <div className="yyyy">$000</div>
+        <div className="zzzz">$000</div>
+        <div className="aaaaa">$000</div>
+        <div className="bbbbb">$000</div>
+        <div className="ccccc">$000</div>
+        <div className="ddddd">$000</div>
+        <div className="eeeee">&000</div> 
+        <div className="fffff">&000</div> 
+        <div className="ggggg">&000</div> 
+        <div className="hhhhh">&000</div> 
+        <div className="iiiii">&000</div> 
+        <div className="jjjjj">&000</div> 
+        <div className="kkkkk">&000</div> 
+        <div className="lllll">&000</div> 
+        <div className="mmmmm">&000</div> 
+
+
+
+    
+
+        
+</div>
+        
+
+
+
+    </div>
+
+
+    
+
+
+
+
+   );
+ }
+    
+ export default App;
+
+
+
+
